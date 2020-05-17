f842ceed3cce8d64c309234425ac30604a05f9dc
Alisson Suzigan <alisson.suzigan@gmail.com>
2020-05-17 10:56:03 -0300

    Update shelf spacings

--- src/components/shelf/shelf.tsx
+++ src/components/shelf/shelf.tsx
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 import Typography from '@material-ui/core/Typography';
 import Icon from '@material-ui/core/Icon';
 import ShelfList from './shelf-list';
-import theme from '../../theme';
 import { ShelfData } from './type';
+import theme from '../../theme';
 
 interface Props {
   data: ShelfData;
@@ -23,17 +23,18 @@ const Shelf = ({ data }: Props): JSX.Element => (
 );
 
 const StyledSection = styled.section`
-  margin-bottom: ${theme.spacing(2)}px;
+  margin-bottom: ${theme.spacing(3)}px;
 `;
 
 const StyledTitle = styled(Typography)`
   display: flex;
   align-items: center;
+  margin: 0 ${theme.spacing(2)}px;
 `;
 
 const StyledIcon = styled(Icon)`
   &.material-icons {
-    margin-right: ${theme.spacing(0.5)}px;
+    margin-right: ${theme.spacing()}px;
     font-size: 1.8rem;
   }
 `;
