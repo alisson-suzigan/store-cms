df8393f14bfa958c492575238ee4ce8809594ce0
Alisson Suzigan <alisson.suzigan@gmail.com>
2020-05-25 15:38:58 -0300

    Add ignore pattern info and remove disable comment

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 module.exports = {
   siteMetadata: {
     title: `Store CMS`,
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,7 +4,6 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-/* eslint-disable */
 import React from 'react';
 import Layout from './src/components/layout';
 
